da6e10f72453ead55bb96740eb7d021fc406d3c4
Nicolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2020-02-26 22:34:13 -0300

    feat: add path util
    - handles path related concerns

---
+++ src/utils/path.ts
@@ -0,0 +1,36 @@
+import { QuerystringParameter } from './types';
+
+/**
+ * Checks for the querystrings of a given path returning the path itself and
+ * each parameter separated;
+ * @param path - Given path of a file
+ *
+ * @returns the path and its querystrings parameters;
+ */
+const path_handler = (path: string): {
+  path: string,
+  parameters: QuerystringParameter[]
+} => {
+  const [resource, parameters_list] = path.split('?');
+  const parameters: QuerystringParameter[] = [];
+
+  if (parameters_list) {
+    parameters_list.split('&').forEach((entity) => {
+      const [param_name, param_value] = entity.split('=');
+      const isIncluded = parameters.filter((param) => param.name === param_name).length > 0;
+      if (!isIncluded) {
+        parameters.push({
+          name: param_name,
+          value: param_value,
+        });
+      }
+    });
+  }
+
+  return {
+    path: resource,
+    parameters,
+  };
+};
+
+export default path_handler;
